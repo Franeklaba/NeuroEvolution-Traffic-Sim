ae0e24442944627abb9a02c86da16fb37234c607
Franciszek Łaba <franeklaba3@gmail.com>
2026-05-26 01:49:44 +0200

    set

--- neuroevolutionconfig.py
+++ neuroevolutionconfig.py
@@ -4,9 +4,9 @@ from dataclasses import dataclass, field
 @dataclass(frozen=True)
 class NeuroevolutionConfig:
 
-    ml_input_type:int = 3
+    ml_input_type:int = 4
 
-    generations: int = 1200
+    generations: int = 2200
     base_mutation_rate: float = 0.3
     base_mutation_strength: float = 0.3
     stability_penalty_weight:float = 0.6
